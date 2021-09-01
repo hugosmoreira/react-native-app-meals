0923653cd05bc3f8de9d681deffc232ad5950334
hugosmoreira <hugo.n4@gmail.com>
2021-09-01 00:34:37 -0700

    new commit

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+
+
+const isAndroid = Platform.OS == 'android'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={{ flex: 1}}>
+        <View style={styles.search}>
+          <Searchbar />        
+        </View>
+        <View style={styles.list}>
+          <Text>Search</Text>        
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
+     
+    
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
   },
 });
